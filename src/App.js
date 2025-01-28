f4a8d25302b3ad496f79d591152b0bdfa5924c2b
théau Yapi <theau.yapi@ynov.com>
2025-01-28 11:36:09 +0100

    remove loader

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import AppRoutes from './Routes';
-import { LoaderProvider } from './components/LoaderContext/LoaderContext';
 import './styles/index.css';
 
 const App = () => {
   return (
     <div className="App">
-      <LoaderProvider>
+
         <AppRoutes />
-      </LoaderProvider>
     </div>
   );
 };
